49e61b124c580b0dc11aa5b72e2ae61b6a4b3d99
parveen99 <40237237+parveen99@users.noreply.github.com>
2019-05-19 17:44:52 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+#Fizzbuzz
+for i in range(1,101):
+    if(i%3==0 and i%5==0):
+        print(i,":Fizzbuzz")
+    elif(i%3==0):    
+        print(i,":Fizz")
+    elif(i%5==0):    
+        print(i,":Buzz")
+    else:
+        print(i)
